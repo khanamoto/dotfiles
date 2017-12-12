8ce0ea9879ab700232f8289f802c86bbb4d5b1a8
Kenji Hanamoto <khanamoto10@gmail.com>
2017-12-12 16:20:31 +0900

    initial commit

---
+++ etc/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -sf ~/dev/src/github.com/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dev/src/github.com/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dev/src/github.com/dotfiles/.zshrc ~/.zshrc
