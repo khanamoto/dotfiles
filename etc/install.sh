b47343632135049130c7bfb5e65db5c55363dc7f
Kenji Hanamoto <khanamoto10@gmail.com>
2018-10-01 22:49:57 +0900

    refactor: Update shell

--- etc/install.sh
+++ etc/install.sh
@@ -4,5 +4,5 @@ ln -sf ~/dev/src/github.com/khanamoto/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dev/src/github.com/khanamoto/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dev/src/github.com/khanamoto/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dev/src/github.com/khanamoto/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dev/src/github.com/khanamoto/dotfiles/.dein.toml ~/.dein.toml
-ln -sf ~/dev/src/github.com/khanamoto/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
+ln -sf ~/dev/src/github.com/khanamoto/dotfiles/.dein.toml ~/.cache/dein/.dein.toml
+ln -sf ~/dev/src/github.com/khanamoto/dotfiles/.dein_lazy.toml ~/.cache/dein/.dein_lazy.toml
